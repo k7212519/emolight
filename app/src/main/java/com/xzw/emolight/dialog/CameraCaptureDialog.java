0ae3eb71742de9c70da86a886ef7d0eb0358a7d4
7212519 <791538283@qq.com>
2018-11-01 04:32:05 +0800

    11_01_04.30 扫描动画基本完成 为cardview两个button加了按下效果

--- app/src/main/java/com/xzw/emolight/dialog/CameraCaptureDialog.java
+++ app/src/main/java/com/xzw/emolight/dialog/CameraCaptureDialog.java
@@ -1,5 +1,6 @@
 package com.xzw.emolight.dialog;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -7,8 +8,15 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.wonderkiln.camerakit.CameraKit;
 import com.wonderkiln.camerakit.CameraView;
@@ -21,7 +29,10 @@ public class CameraCaptureDialog extends DialogFragment {
     //camera属性：使用前置镜头
     private int cameraFace = CameraKit.Constants.FACING_FRONT;
     private boolean cropOutput = true;
+    private Button buttonScan;
+    private RelativeLayout layoutScan;
     private CameraView camera;
+    private ImageView imageViewScanLine;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,8 +42,11 @@ public class CameraCaptureDialog extends DialogFragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_fragment_camera_capture, container, false);
+        imageViewScanLine = view.findViewById(R.id.img_scan_line);
+        buttonScan = view.findViewById(R.id.btn_scan);
+        layoutScan = view.findViewById(R.id.layout_scan);
         camera = view.findViewById(R.id.camera_kit_in_dialog);
         camera.setMethod(cameraMethod);
         camera.setCropOutput(cropOutput);
@@ -43,6 +57,13 @@ public class CameraCaptureDialog extends DialogFragment {
          */
         getDialog().setCancelable(false);
         getDialog().setCanceledOnTouchOutside(false);
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnim(imageViewScanLine);
+            }
+        });
 
 
         return view;
@@ -59,4 +80,19 @@ public class CameraCaptureDialog extends DialogFragment {
         super.onPause();
         camera.stop();
     }
+
+
+    /**
+     * 扫描线动画
+     * @param view
+     */
+    private void startAnim(View view) {
+        Animation animationLineTranslate = new TranslateAnimation(
+                0, 0, 0, layoutScan.getHeight()*0.64f );
+        animationLineTranslate.setRepeatMode(Animation.REVERSE);
+        animationLineTranslate.setRepeatCount(10000);
+        animationLineTranslate.setDuration(1000);
+        view.startAnimation(animationLineTranslate);
+    }
+
 }
