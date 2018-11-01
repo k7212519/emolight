194e27b5af5af9294cc5c4d64511b35e7176abdd
7212519 <791538283@qq.com>
2018-11-01 16:01:11 +0800

    11_01_16.00 添加了震动

--- app/src/main/java/com/xzw/emolight/dialog/CameraCaptureDialog.java
+++ app/src/main/java/com/xzw/emolight/dialog/CameraCaptureDialog.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -80,6 +81,8 @@ public class CameraCaptureDialog extends DialogFragment {
                     case MotionEvent.ACTION_DOWN:
                         controlLineAnim(imageViewScanLine, ACTION_START);
                         controlFrameAnim(btnScanImgFrame, ACTION_END);
+                        Vibrator vibrator = (Vibrator)getContext().getSystemService(getContext().VIBRATOR_SERVICE);
+                        vibrator.vibrate(11);
                         break;
                     case MotionEvent.ACTION_UP:
                         controlFrameAnim(btnScanImgFrame, ACTION_START);
