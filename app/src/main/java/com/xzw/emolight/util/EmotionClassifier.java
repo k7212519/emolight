c33a9a50ae73b8ef2493901cb9824df901963d54
7212519 <791538283@qq.com>
2018-11-03 07:21:07 +0800

    11_03_07.20 能够找出最大值和index 还得去上电磁波，心累

--- app/src/main/java/com/xzw/emolight/util/EmotionClassifier.java
+++ app/src/main/java/com/xzw/emolight/util/EmotionClassifier.java
@@ -6,6 +6,8 @@ import com.google.gson.Gson;
 import com.xzw.emolight.others.FaceInfo;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class EmotionClassifier {
     private ArrayList<Double> emoResultArrayList = new ArrayList<>(7);
@@ -20,16 +22,15 @@ public class EmotionClassifier {
 
     public void getEmoResult() {
         addEmo(faceInfo);
-        for (int i = 0; i < 7; i++) {
+        /*for (int i = 0; i < 7; i++) {
             Log.d("debug", String.valueOf(emoResultArrayList.get(i)));
-        }
+        }*/
+        String string = Collections.max(emoResultArrayList).toString();
+        //0 happiness  1 Neutral  2 Surprise  3 Sadness  4 Fear  5 Disgust  6 Anger
+        int i = emoResultArrayList.indexOf(Collections.max(emoResultArrayList));
+        Log.d("debug", "最大值：List"+i+"为"+string);
+
     }
-
-    /*public void test() {
-        String s = String.valueOf(faceInfo.getFaces().get(0).getAttributes().getEmotion().getAnger());
-
-    }*/
-
     /**
      * 将情绪值添加进ArrayList
      * @param faceInfo
